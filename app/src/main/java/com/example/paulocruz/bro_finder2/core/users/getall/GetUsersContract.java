f952c2799098cc41abc53ed264ce1021c0a923e9
tgpsi407paulo <tgpsi407paulo@gmail.com>
2017-07-02 18:54:41 +0100

    Copy

---
+++ app/src/main/java/com/example/paulocruz/bro_finder2/core/users/getall/GetUsersContract.java
@@ -0,0 +1,42 @@
+package com.example.paulocruz.bro_finder2.core.users.getall;
+
+import com.delaroystudios.firebasechat.models.User;
+
+import java.util.List;
+
+
+public interface GetUsersContract {
+    interface View {
+        void onGetAllUsersSuccess(List<User> users);
+
+        void onGetAllUsersFailure(String message);
+
+        void onGetChatUsersSuccess(List<User> users);
+
+        void onGetChatUsersFailure(String message);
+    }
+
+    interface Presenter {
+        void getAllUsers();
+
+        void getChatUsers();
+    }
+
+    interface Interactor {
+        void getAllUsersFromFirebase();
+
+        void getChatUsersFromFirebase();
+    }
+
+    interface OnGetAllUsersListener {
+        void onGetAllUsersSuccess(List<User> users);
+
+        void onGetAllUsersFailure(String message);
+    }
+
+    interface OnGetChatUsersListener {
+        void onGetChatUsersSuccess(List<User> users);
+
+        void onGetChatUsersFailure(String message);
+    }
+}
